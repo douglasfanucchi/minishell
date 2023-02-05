f2c96e96c1bb17d9c5446919d536c0362035ade0
Douglas Fanucchi <douglasfanucchi@gmail.com>
2023-02-05 18:34:45 +0100

    remove unused functions

--- sources/redirects/redirect_file_descriptor.c
+++ sources/redirects/redirect_file_descriptor.c
@@ -12,34 +12,6 @@
 
 #include <minishell.h>
 
-static int	read_file(char *filename, int flags)
-{
-	if (access(filename, F_OK) != 0)
-	{
-		printf("%s: No such file or directory.\n", filename);
-		return (-1);
-	}
-	if (access(filename, R_OK) != 0)
-	{
-		printf("%s: Permission denied.\n", filename);
-		return (-1);
-	}
-	return (open(filename, flags));
-}
-
-static int	create_file(char *filename, int flags, mode_t mode)
-{
-	int	fd;
-
-	fd = open(filename, flags, mode);
-	if (fd == -1)
-	{
-		printf("Could not create %s: Permission denied.\n", filename);
-		return (-1);
-	}
-	return (fd);
-}
-
 int	ft_get_redirect_file_descriptor(t_list *node)
 {
 	t_token	*token;
@@ -57,10 +29,10 @@ int	ft_get_redirect_file_descriptor(t_list *node)
 	if (*filename == '\n')
 		return (-1);
 	if (ft_strncmp(redirect, ">", 2) == 0)
-		return (create_file(filename, O_CREAT | O_TRUNC | O_WRONLY, 0644));
+		return (open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0644));
 	if (ft_strncmp(redirect, ">>", 3) == 0)
-		return (create_file(filename, O_CREAT | O_APPEND | O_WRONLY, 0644));
+		return (open(filename, O_CREAT | O_APPEND | O_WRONLY, 0644));
 	if (ft_strncmp(redirect, "<", 2) == 0)
-		return (read_file(filename, O_RDONLY));
+		return (open(filename, O_RDONLY));
 	return (create_here_doc(here_doc_filename(), token->original));
 }
