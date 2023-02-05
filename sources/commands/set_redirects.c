ae56cf68197bc7ab29595fe225e1bf8f155805e0
Douglas Fanucchi <douglasfanucchi@gmail.com>
2023-02-05 17:42:22 +0100

    add error to message when redirect fails

--- sources/commands/set_redirects.c
+++ sources/commands/set_redirects.c
@@ -12,10 +12,32 @@
 
 #include <minishell.h>
 
-static void	del_redirection_nodes(t_list *node)
+static void	del_redirection_nodes(t_list *node, char **argv)
 {
 	ft_lstdel(node, ft_del_token);
 	ft_lstdel(node, ft_del_token);
+	ft_remove_arg(argv, *argv);
+	ft_remove_arg(argv, *argv);
+}
+
+static char	validate_redirect(t_command *command, t_redirect *redirect,
+		t_list *node)
+{
+	char	*error_msg;
+	t_token	*token;
+
+	if (redirect->old_fd != -1)
+		return (1);
+	token = node->next->content;
+	if (access(token->original, F_OK) != 0)
+		error_msg = ": No such file or directory\n";
+	else if (access(token->original, X_OK) != 0)
+		error_msg = ": Permission denied\n";
+	ft_lstadd_back(command->errors, ft_lstnew(
+			ft_strjoin(token->original, error_msg)));
+	command->bash_status = 1;
+	ft_del_redirect((void *)redirect);
+	return (0);
 }
 
 void	ft_set_command_redirects(t_command *command)
@@ -40,9 +62,9 @@ void	ft_set_command_redirects(t_command *command)
 		}
 		redirect = ft_new_redirect(node);
 		redirect->old_fd = ft_get_redirect_file_descriptor(node);
+		if (!validate_redirect(command, redirect, node))
+			break ;
 		ft_lstadd_back(command->redirects, ft_lstnew(redirect));
-		ft_remove_arg(argv, *argv);
-		ft_remove_arg(argv, *argv);
-		del_redirection_nodes(node);
+		del_redirection_nodes(node, argv);
 	}
 }
