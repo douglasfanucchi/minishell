332703b79b9cad998e6529356abe7305448d99cf
Douglas Fanucchi <douglasfanucchi@gmail.com>
2023-02-09 10:14:19 -0300

    fixes commands validation

--- sources/executor/executor.c
+++ sources/executor/executor.c
@@ -12,30 +12,34 @@
 
 #include <minishell.h>
 
-static void	validate_commands_syntax(void *content)
-{
+static char	validate_commands(t_list *node)
+{	
 	t_command	*command;
 	char		*unexpected_token;
 
-	if (g_minishell.status != 0)
-		return ;
-	command = content;
-	unexpected_token = ft_analyse_command(command);
-	if (unexpected_token)
+	while (node)
 	{
-		if (ft_strncmp(unexpected_token, "\n", 2) == 0)
-			unexpected_token = "newline";
-		printf("syntax error near unexpected token `%s`\n",
-			unexpected_token);
-		g_minishell.status = 2;
+		command = node->content;
+		unexpected_token = ft_analyse_command(command);
+		if (unexpected_token)
+		{
+			if (ft_strncmp(unexpected_token, "\n", 2) == 0)
+				unexpected_token = "newline";
+			printf("syntax error near unexpected token `%s`\n",
+				unexpected_token);
+			g_minishell.status = 2;
+			return (0);
+		}
+		node = node->next;
 	}
+	return (1);
 }
 
 void	ft_executor(t_list **commands)
 {
-	ft_lstiter(*commands, validate_commands_syntax);
-	if (g_minishell.status == 0 && *commands)
-		ft_exec_commands(commands);
+	if (!*commands || !validate_commands(*commands))
+		return ;
+	ft_exec_commands(commands);
 	ft_lstclear(commands, ft_del_command);
 	free(commands);
 }
