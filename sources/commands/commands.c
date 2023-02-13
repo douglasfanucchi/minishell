6bb9a5093af62ded41e638bad7adcea6c5276d79
Douglas Fanucchi <douglasfanucchi@gmail.com>
2023-02-13 17:20:28 -0300

    adjust pipe searching to separate command

--- sources/commands/commands.c
+++ sources/commands/commands.c
@@ -3,14 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   commands.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dfanucch <dfanucch@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: dfanucch <dfanucch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 08:23:53 by dfanucch          #+#    #+#             */
-/*   Updated: 2023/02/09 08:23:54 by dfanucch         ###   ########.fr       */
+/*   Updated: 2023/02/13 17:17:35 by dfanucch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
+
+static char	*ft_search_pipe(char *input)
+{
+	char	quoted;
+
+	quoted = 0;
+	while (*input)
+	{
+		if (ft_is_quote(*input))
+		{
+			quoted = *input;
+			input++;
+			continue ;
+		}
+		if (*input == '|' && !quoted)
+			return (input);
+		if (quoted && quoted == *input)
+			quoted = 0;
+		input++;
+	}
+	return (NULL);
+}
 
 t_list	**ft_commands(char *input)
 {
@@ -21,7 +43,7 @@ t_list	**ft_commands(char *input)
 	commands = ft_newlist();
 	while (*input)
 	{
-		end_input = ft_strchr(input, '|');
+		end_input = ft_search_pipe(input);
 		if (!end_input)
 			end_input = input + ft_strlen(input);
 		input_substr = ft_substr(input, 0, end_input - input);
